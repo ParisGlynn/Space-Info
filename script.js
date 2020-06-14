30129b4f8574069272f4c53b4e6529006a0fde9d
Paris Glynn <Parisglynn1@gmail.com>
2020-06-14 20:55:21 +0100

    Add missions to DOM and style

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const spaceList = document.getElementById('space-list');
+
 fetch('https://spacelaunchnow.me/api/3.3.0/launch/upcoming/')
 
   .then(res => res.json())
-  .then(data => console.log(data));
+  .then(data => addDataToDOM(data));
+
+function addDataToDOM(data) {
+  console.log(data);
+  data.results.forEach(item => {
+    console.log(item.name);
+    const spaceItem = document.createElement('div');
+    spaceItem.classList.add('item-box');
+    spaceItem.innerHTML = `
+      <span class = 'mission'>Mission: ${item.name}</span>
+      <span class = 'net'>NET: ${item.net}</span>`;
+    spaceList.appendChild(spaceItem);
+  });
+}
